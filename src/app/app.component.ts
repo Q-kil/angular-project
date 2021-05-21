554bf5317baa8aed61b72ba392e40730ad0a6813
聂开发 <niekaifa@apowo.com>
2021-05-21 17:47:55 +0800

    proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-project';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+  ngOnInit() {
+    this.http.get(`/cdn/5e719a0a68196e416ecf7aad/.config.json?t=1621504450293`).subscribe(res => {
+      console.log('res', res);
+    });
+  }
 }
